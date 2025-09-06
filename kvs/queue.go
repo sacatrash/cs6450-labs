8bc62e449a52ef3e509901401a80ddee791362b8
Andy Herbert <andyman1222@users.noreply.github.com>
2025-09-06 02:27:08 -0600

    fixed some stuff

--- kvs/queue.go
+++ kvs/queue.go
@@ -11,7 +11,7 @@ type Queue struct {
 	name      string
 }
 
-func (n Queue) AddTask(request *RequestBatch, response *ResponseBatch) *chan error {
+func (n *Queue) AddTask(request *RequestBatch, response *ResponseBatch) *chan error {
 	tmp := make(chan error)
 	n.channels = append(n.channels, tmp)
 	n.requests = append(n.requests, request)
@@ -19,21 +19,21 @@ func (n Queue) AddTask(request *RequestBatch, response *ResponseBatch) *chan err
 	return &tmp
 }
 
-func (n Queue) next() *Queue {
+func (n *Queue) next() *Queue {
 	if n.nextNode == nil {
-		return &n
+		return n
 	}
 	return n.nextNode
 }
 
-func (n Queue) addNew(name string) *Queue {
+func (n *Queue) addNew(name string) *Queue {
 	tmp := new(Queue)
 	tmp.name = name
 	tmpNext := n.nextNode
 	n.nextNode = tmp
 	tmp.nextNode = tmpNext
 	if tmpNext == nil || tmpNext == tmpNext.nextNode {
-		tmpNext = &n
+		tmpNext = n
 	}
 	return tmp
 }
@@ -58,27 +58,48 @@ func (m *MasterQueue) Initialize(lock *sync.Mutex, target *map[string]string) {
 	m.TargetMap = target
 	m.locker = lock
 	m.MaxElements = 10
+
 }
 
 func (m *MasterQueue) AddNewQueue(name string) *Queue {
+
+	if len(m.nodeList) > 0 {
+		m.Node2queue[name] = m.Node2queue[m.nodeList[len(m.nodeList)-1]].addNew(name)
+	} else {
+		m.Node2queue[name] = new(Queue)
+		m.Node2queue[name].name = name
+	}
 	m.nodeList = append(m.nodeList, name)
-	m.Node2queue[name] = m.Node2queue[m.nodeList[len(m.nodeList)-1]].addNew(name)
 	return m.Node2queue[name]
 }
 
 func (m *MasterQueue) Process() {
-	m.currentNode = m.currentNode.next()
-	m.locker.Lock()
-	defer m.locker.Unlock()
-	m.currentNode.process(m.MaxElements, m.TargetMap)
+	if m.currentNode == nil {
+		if len(m.nodeList) > 0 {
+			m.currentNode = m.Node2queue[m.nodeList[0]]
+		} else {
+			return
+		}
+	} else {
+		m.currentNode = m.currentNode.next()
+	}
+
+	if m.currentNode != nil {
+		m.locker.Lock()
+		defer m.locker.Unlock()
+		m.currentNode.process(m.MaxElements, m.TargetMap)
+	}
 }
 
 //processes elements in the queue, and updates priorities
 func (q *Queue) process(maxElements int, mp *map[string]string) {
 	for i := 0; i < maxElements && len(q.channels) > 0; i++ {
-		req := q.requests[1:][0]
-		ch := q.channels[1:][0]
-		res := q.responses[1:][0]
+		req := q.requests[0]
+		q.requests = q.requests[1:]
+		ch := q.channels[0]
+		q.channels = q.channels[1:]
+		res := q.responses[0]
+		q.responses = q.responses[1:]
 		for i, op := range req.Ops {
 			if op.IsRead {
 				if v, ok := (*mp)[op.Key]; ok {
